c53301ea581136f992eab4ba013e98bb1a568ed3
TroianB <136427600+TroianB@users.noreply.github.com>
2026-08-09 15:57:37 +1200

    Copy React entry point

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import BeekeepingApp from "./App.jsx";
+import "./fit-screen.css";
+import "./theme.js";
+import "./csv-controls.js";
+import "./apiary-mobile.js";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BeekeepingApp />
+  </React.StrictMode>
+);
