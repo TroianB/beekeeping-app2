22ac4ce7a592cf784147281992b9c8e515f6fe6a
TroianB <136427600+TroianB@users.noreply.github.com>
2026-08-19 22:13:59 +1200

    Load apiary list-only mode

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import { createRoot } from "react-dom/client";
 import MonitoringShell from "./MonitoringShell.jsx";
 import "./fit-screen.css";
 import "./apiary-column-order.css";
+import "./apiary-list-only.css";
 import "./theme.js";
 import "./csv-controls.js";
 import "./apiary-mobile.js";
+import "./apiary-list-only.js";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
