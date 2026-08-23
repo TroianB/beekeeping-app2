6afc7754f9f7ac7634842764e35967256827fba3
TroianB <136427600+TroianB@users.noreply.github.com>
2026-08-23 20:04:35 +1200

    Load compact apiary list top behavior

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import "./apiary-edit-saved-highlight.css";
 import "./apiary-info-screen.css";
 import "./apiary-list-screen-text.css";
 import "./apiary-edit-screen-text.css";
+import "./apiary-list-compact-top.css";
 import "./theme.js";
 import "./csv-controls.js";
 import "./apiary-mobile.js";
@@ -17,6 +18,7 @@ import "./apiary-list-only.js";
 import "./apiary-edit-fix.js";
 import "./apiary-edit-cancel-return.js";
 import "./apiary-list-metric-dropdown.js";
+import "./apiary-list-compact-top.js";
 import "./dashboard-apiary-swipe.js";
 
 createRoot(document.getElementById("root")).render(
